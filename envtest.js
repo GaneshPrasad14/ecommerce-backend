9d4a93914987fdca8619385055a45b3312a6863f
GaneshPrasad14 <128919436+GaneshPrasad14@users.noreply.github.com>
2025-07-17 11:00:59 +0530

    Delete envtest.js

--- envtest.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-console.log('File exists:', fs.existsSync(__dirname + '/.env'));
-console.log('File contents:\n', fs.readFileSync(__dirname + '/.env', 'utf8'));
-
-require('dotenv').config({ path: __dirname + '/.env' });
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('All env:', process.env);
