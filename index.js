75f89d0433469ec548edb4c0bee0727a9a2a3934
GaneshPrasad14 <128919436+GaneshPrasad14@users.noreply.github.com>
2025-07-17 11:52:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://saigame.in',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
